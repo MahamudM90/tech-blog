efa84bc8fe54a8c2b8a969bb1bb3286c31eb5c19
Mahamud Hasan <mahamud15-10467@diu.edu.bd>
2023-02-24 22:38:42 +0600

    updated

--- app/TranstackQueryWrapper.tsx
+++
@@ -1,9 +0,0 @@
-"use Client";
-const { QueryClient, QueryClientProvider } = require("@tanstack/react-query");
-const queryClient = new QueryClient();
-const TanstackQueryWrapper = ({ children }) => {
-  return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-  );
-};
-export default TanstackQueryWrapper;
